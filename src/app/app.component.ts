e6c3b93c15e0912f7e602889f76d78d481077304
Your Name <you@example.com>
2024-10-05 14:58:47 +0530

    first commit Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { BannerComponent } from "./pages/banner/banner.component";
+import { SummaryComponent } from './pages/summary/summary.component';
+import { SkillsetComponent } from './pages/skillset/skillset.component';
+import * as AOS from 'aos';
+import { WorkexperienceComponent } from './pages/workexperience/workexperience.component';
+import { ProjectComponent } from './pages/project/project.component';
+import { LinksComponent } from './pages/links/links.component';
+import { HeaderComponent } from './pages/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SummaryComponent, BannerComponent, SkillsetComponent, WorkexperienceComponent, ProjectComponent, LinksComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+
   title = 'uthayaportfolio';
+  ngOnInit() {
+      AOS.init();
+  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { BannerComponent } from './pages/banner/banner.component';
+import { ResumeComponent } from './pages/resume/resume.component';
+import { SkillsetComponent } from './pages/skillset/skillset.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path:"",
+    component:BannerComponent
+  },
+  {
+    path:"resume",
+    component: ResumeComponent
+  },
+  {
+    path: "home",
+    component: SkillsetComponent
+  },
+
+];
